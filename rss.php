c64f34147a31b286f5e5103ffde67390e9498024
Albert Suàrez <albert.suarez.molgo@est.fib.upc.edu>
2016-03-01 11:57:04 +0100

    tasca #6 casi finalitzada

--- rss.php
+++ rss.php
@@ -1,5 +1,29 @@
 <?php
 
-echo 'Wait until Task #6 is done to get something interesting here';
+require_once("DbHandler.php");
+
+setlocale(LC_TIME,"en_US");
+
+$dbhandler = new DbHandler();
+
+$resp = new SimpleXMLElement("<rss></rss>");
+$resp->addAttribute('version', '2.0');
+$resp->addChild('channel');
+$resp->channel->addChild('title', 'Wall Of Tweets 2 - RSS Version');
+$resp->channel->addChild('link', 'http://localhost:8080/waslab02/wall.php');
+$resp->channel->addChild('description', 'RSS 2.0 feed that retrieves the tweets posted to the web "Wall of Tweets 2"');	
+	
+$res = $dbhandler->getTweets();
+foreach($res as $tweet) {
+	$item = $resp->channel->addChild('item');
+ 	$item->addChild('title', $tweet['text']);
+	$item->addChild('link', 'http://localhost:8080/waslab02/wall.php#item_'.$tweet['id']);
+	$description = 'This is WoT tweet #', $tweet['id'], ' posted by ', $tweet['author'], '. It has been liked by ', $tweet['likes'], 'people.';
+	$item->addChild('description', $description);
+	$item->addChild('pubDate',date(DATE_W3C,$tweet['time']));
+}
+
+header('Content-type: text/xml');
+echo $resp->asXML();
 
 ?>
