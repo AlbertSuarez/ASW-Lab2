14442d5c78d741a28e27fed2b6056e6bb56c6195
Albert Suàrez <albert.suarez.molgo@est.fib.upc.edu>
2016-03-01 10:47:46 +0100

    tasca #3 finalitzada

--- client.php
+++ client.php
@@ -3,6 +3,16 @@
 $URI = 'http://localhost:8080/waslab02/wall.php';
 $resp = file_get_contents($URI);
 echo $http_response_header[0], "\n"; // Print the first HTTP response header
-echo $resp;  // Print HTTP response body
+//echo $resp;  // Print HTTP response body
+
+$alltweets = new SimpleXMLElement($resp);
+
+foreach ($alltweets as $tweet) {
+	echo '[tweet #', $tweet['id'], '] ', $tweet->author, ': ', $tweet->text, ' [', $tweet->time, ']', "\n";
+		
+
+
+
+}
 
 ?>
